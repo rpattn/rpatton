cec45a67cbc9cd1e284dc331c734953811a95e7f
CrazyRexxar <robpatton04@gmail.com>
2023-09-25 16:47:45 +0100

    FEAv2 Added
    Added as static page - also improved project page with flex box

--- src/app/projects/ProjectText.js
+++ src/app/projects/ProjectText.js
@@ -20,21 +20,31 @@ const ProjectText = () => {
             name: "My Projects",
             desc: "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.",
             link: "none",
+            anim: "projectText"
         },
         {
             name: "Search Map",
-            desc: "Leverages Google's Maps API and cloud services to enable users to search for places by name, type and location. This data can then be filtered, saved to the cloud or exported.",
-            link: "#searchmap"
+            desc: "Leverages Google's Maps API and cloud services to enable users to search for places by name, type and location. This data can then be filtered, saved to the cloud or exported for personal use.",
+            link: "#searchmap",
+            anim: "projectText2"
         },
         {
             name: "Daring Dash",
             desc: "Led a team that designed and manufactured an autonomous buggy. Optimised the vehicles suspension response using Matlab to evaluate a kinematic model constructed from 1st Principles.",
             link: "#daringdash",
+            anim: "projectText"
+        },
+        {
+            name: "FEA Sandbox",
+            desc: "Browser based playground. Construct 2D node element models, apply forces, boundary conditions, and calculate the structure's response. Constructs intermediate matricies to help students visualise the finite element method.",
+            link: "#feasandbox",
+            anim: "projectText2"
         },
         {
             name: "Volunteering",
             desc: "Travelled to Tanzania to participate in several aid projects, including Civil Engineering work redirecting rainwater around a local school, refrbishing desks and working with children",
-            link: "#project3",
+            link: "#volunteering",
+            anim: "projectText1"
         },
     ]
 
@@ -42,6 +52,7 @@ const ProjectText = () => {
         {text1: "", text2: "", text3: "", extLink: ""},
         {text1: "Built using HTML, CSS, JS", text2: "Powered by React, Firebase, Google APIs", text3: "Responsive design", extLink: "https://searchmap.web.app/"},
         {text1: "Computer aided, analysis led design.", text2: "Project Managed team", text3: "Achieved 86% in this coursework", extLink: "/cv#daringdash"},
+        {text1: "Designed as an Educational tool", text2: "First in a family of Engineering visualisation tools I am working on", text3: "Open source, free to use", extLink: "/fea/index.html"},
         {text1: "Fund raised over 2 years", text2: "Collaborated with a diverse team", text3: "Harboured a love for travel", extLink: ""},
     ]
 
@@ -71,7 +82,13 @@ const ProjectText = () => {
                      {src: "/buggy/buggy3.png", h:1340, w:1340}]
         },
         {
-            link: "tanzania",
+            link: "feasandbox",
+            images: [{src: "/fea/fea1.png", h:1218, w:688},
+                     {src: "/fea/fea2.png", h:560, w:600},
+                     {src: "/fea/fea3.png", h:826, w:826}]
+        },
+        {
+            link: "volunteering",
             images: [{src: "/tanzania/tanzania3.png", h:1062, w:681},
                      {src: "/tanzania/tanzania1.png", h:654, w:654},
                      {src: "/tanzania/tanzania2.png", h:654, w:654}]
@@ -96,7 +113,7 @@ const ProjectText = () => {
 
     //assign a ref and in view hook for each image card in imageInfo
     imagesInfo.forEach(prj => {
-        var {ref, inView } = useInView({threshold: 0.35})
+        var {ref, inView } = useInView({threshold: 0.5})
         prj.prjRef = ref
         prj.isVisible = inView
     });
@@ -107,8 +124,60 @@ const ProjectText = () => {
         setShow(true)
     })
 
-    return (
-        (ready) ? 
+    return ((ready) ? 
+    <div className="md:grid md:grid-cols-2"><div ref={pageBorder} className="h-8"></div> 
+        <div className="col-start-1 col-span-1 hidden sm:block projextFlex" >
+            <div className={`xl:pl-8 lg:pl-0 md:pl-4 ${(borderInView)? "" : "md:fixed top-8 "} ${isVis? "" : "hidden"} xl:max-w-md lg:max-w-sm lg:pr-6 md:pr-8 md:max-w-sm`}>
+            <h1 className={` xl:text-6xl md:text-5xl xl:mt-16 lg:mt-16 md:mt-8 font-bold tracking-tight text-gray-900 dark:text-gray-200 ${(scrollY<1)? projectInfo[scrollY].anim : ""}`}>{projectInfo[scrollY].name}</h1>
+            <p class={`${projectInfo[scrollY].anim} mt-4 ml-1 text-lg leading-8 text-gray-600 dark:text-gray-200`}>{projectInfo[scrollY].desc}</p>
+            {(projectBullets[scrollY].text1 != "") ? 
+            <><ul className={`${projectInfo[scrollY].anim} transition-all md:mt-4 ml-8 list-disc `}>
+                <li className="text-lg xl:mb-1 text-gray-700 dark:text-gray-200">{projectBullets[scrollY].text1}</li>
+                <li className="text-lg xl:mb-1 text-gray-700 dark:text-gray-200">{projectBullets[scrollY].text2}</li>
+                <li className="text-lg xl:mb-1 text-gray-700 dark:text-gray-200">{projectBullets[scrollY].text3}</li>
+            </ul>
+            <button href={projectBullets[scrollY].extLink}  className={` mt-6 z-50 ml-2 pl-4 pr-4 pt-2 pb-2 outline outline-1 outline-blue-500 rounded-lg flex text-xl font-bold dark:text-gray-300 dark:outline-indigo-600 text-blue-700 drop-shadow-2xl`}>
+                <Link href={projectBullets[scrollY].extLink}> Check it out</Link> <RxExternalLink className="projectButton animate-pulse mt-1 ml-2" /></button>
+            </>
+            : <></>}
+            <ul className={`flex transition-all flex-wrap mt-4 ${(scrollY < 1)? "projectText": "hidden xl:flex"} `}>
+            {projectInfo.splice(1).map(({ name, link }, index) => (  //remove first spalsh page, then add links
+                <li key={index} className={`${(scrollY==(index+1))? "shadow-xl dark:ring-indigo-600" : ""} rounded-full transition-all ml-1 mr-1 mt-3 mb-1 px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:ring-gray-500 dark:hover:dark:bg-gray-900`}>
+                <a href={link} class="font-semibold text-blue-600 dark:text-gray-400">{name}  </a>
+                </li> 
+            ))}
+            </ul>
+            </div>
+        </div>
+
+        <div className="md:col-start-2 md:col-span-1 md:block hidden">
+            {imagesInfo.map(({ images, prjRef, isVisible, link }, index) => (
+            <div ref={prjRef} id={link} key={index} 
+                className={`p-4 pt-8 transition-all mb-20
+                ${(isVisible && (index>0))?         //if visible and not first card, check card above then render, else render if visible
+                    (imagesInfo[index-1].isVisible)? "" : "md:shadow-xl md:scale-105 md:-translate-x-3 lg:translate-x-0  " 
+                : (isVisible)? "md:shadow-xl" : ""}`}> 
+
+            <Image unoptimized={true} src={images[0].src} style={imageStyle} width={images[0].w} height={images[0].h} alt="todo"/>
+            <div className="grid grid-cols-2">
+                <div className="col-start-1 col-span-1">
+                    <Image src={images[1].src} style={imageStyle} width={images[1].w} height={images[1].h} alt="todo"/></div>
+                <div className="col-start-2 col-span-1">
+                    <Image src={images[2].src} style={imageStyle} width={images[2].w} height={images[2].h} alt="todo"/></div>
+                </div>
+            </div>
+            ))}
+
+            <div className="md:h-28 lg:h-16"></div>
+        </div>
+    </div>
+    :<></>)
+}
+
+export default ProjectText;
+
+/*
+(ready) ? 
         <div className="md:grid md:grid-cols-2"><div ref={pageBorder} className="h-8"></div> 
         <div className="col-start-1 col-span-1 p-6 transition-all hidden sm:block" >
             <h1 className={`text-4xl ml-3 md:mt-10 lg:mt-12 transition-all ${(borderInView)? "" : "md:fixed top-10 "} ${isVis? "" : "hidden"} font-bold tracking-tight text-gray-900 sm:text-6xl md:max-w-xs xl:max-w-none md:pr-12 pr-0  sm:max-w-none dark:text-gray-200`}>{projectInfo[scrollY].name}</h1>
@@ -165,10 +234,7 @@ const ProjectText = () => {
         </div>
         </div>
         : <></>
-    )
-}
-
-export default ProjectText;
+        */
 
 /*
 <h1 className="text-4xl ml-3 md:mt-12 lg:mt-12 md:fixed font-bold tracking-tight text-gray-900 sm:text-6xl xl:max-w-md lg:max-w-sm pr-12 md:max-w-sm sm:max-w-none">{projectInfo[scrollY].name}</h1>
